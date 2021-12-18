efea35f2a5106956fbe06b20077bfd9afd64c855
chuks2121 <chukylofashionstar@gmail.com>
2021-12-17 20:59:09 -0800

    added

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,18 @@
-!/usr/bin/python3
-"""A function that prints a square"""
+#!/usr/bin/python3
+"""
+Module 0-add_integer
+Contains one method that returns an int sum
+Accepts two values, whether int or float, and casts them to int before adding
+"""
 
 
 def add_integer(a, b=98):
-    """Function adds two number provided they are integers
-        if they are floats, they are typecasted into integers first
-    Args:
-        a (int): First first interger to be supplied
-        b (int): second integer to be supplied
-    Returns: Returns a + b
     """
-    if isinstance(a, int) or isinstance(a, float):
-        if isinstance(b, int) or isinstance(b, float):
-            return int(a + b)
-        else:
-            raise TypeError("b must be an integer")
-    else:
+    Returns a + b as int
+    """
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
